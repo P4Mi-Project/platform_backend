fb22849f122f92d1c03c8956e1442b20ea091aaf
Md Masud karim <msmasud578@gmail.com>
2025-05-05 00:27:58 +0200

    Added http exception to the langauge route

--- services/LanguageService.py
+++ services/LanguageService.py
@@ -1,5 +1,6 @@
 from serializers import serializer
 from configs.firebase_admin_config import db
+from fastapi.exceptions import HTTPException
 
 class LanguageService:
     def __init__(self):
@@ -10,11 +11,15 @@ class LanguageService:
             return [serializer.Language(name = item.to_dict()["name"]) for item in db.collection("languages").get()]
         except:
             import traceback; traceback.print_exc();
-            return []
+            # return []
+            raise HTTPException(status_code=500, detail=f"Something went wrong while trying to save the course info the database. Please have a look at the log.")
+            
         
     def get_language_by_id(self,lang_id:str) -> serializer.Language | None:
         try:
             return db.collection("languages").document(lang_id).get().to_dict()
         except:
             import traceback; traceback.print_exc();
-            return None
+            # return None
+            raise HTTPException(status_code=500, detail=f"Something went wrong while trying to save the course info the database. Please have a look at the log.")
+            
